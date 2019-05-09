420dab92f09753e4d40e66b8479397d0efe1daba
Omullo <foo6@cornell.edu>
2019-05-09 10:55:00 -0400

    soo full of errors, but a good start.
    This still needs so much optimization, but I'll just add it here.
    Just thought of this and implemented it on the fly. I'll assume it runs in O(nlogn) with a space of O(1). But does it though??

---
+++ src/pairsThatEqual.java
@@ -0,0 +1,40 @@
+/**
+ *
+ */
+
+/**
+ * @author frankomullo
+ *
+ */
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class pairsThatEqual {
+
+	/**
+	 *
+	 */
+	public static ArrayList<int[]> pairsThatEqualSum(int[] inputArray, int targetSum) {
+		// TODO return an array of all pairs of integers from the input array whose sum
+		// equal the the target
+		Arrays.sort(inputArray);
+		int start = 0;
+		int end = inputArray.length - 1;
+		ArrayList<int[]> arr = new ArrayList<>();
+		while (start < end) {
+			if (inputArray[start] + inputArray[end] == targetSum) {
+				int[] array = { start, end };
+				arr.add(array);
+				start++;
+				end--;
+			}
+			if (inputArray[start] + inputArray[end] < targetSum) {
+				start++;
+			} else if (inputArray[start] + inputArray[end] > targetSum) {
+				end--;
+			}
+
+		}
+		return arr;
+	}
+}
---
+++ src/pairsThatEqualTest.java
@@ -0,0 +1,20 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+class pairsThatEqualTest {
+
+	@Test
+	void testPairsThatEqual() {
+		int[] inputArray = { 1, 2, 3 };
+		int targetSum = 3;
+
+		ArrayList<int[]> output = pairsThatEqual.pairsThatEqualSum(inputArray, targetSum);
+		for (int[] x : output) {
+			assertEquals(x[0] + x[1], targetSum);
+		}
+	}
+
+}
