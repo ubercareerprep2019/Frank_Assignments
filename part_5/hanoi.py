696b2041dfc445e4c11340a6d7a65cd03c0d4e23
Frank Otieno <foo6@cornell.edu>
2019-05-12 00:58:29 -0400

    Most basic implementation, took an easy approach;
    will implement in java later; this is basic, does not follow the instructions given.
    Should just act as guidance.

---
+++ part_5/hanoi.py
@@ -0,0 +1,37 @@
+def solve_hanoi(n, start, goal, temp):
+    """Prints out the instructions for moving n disks (ordered
+    smallest to largest, going down) from the start to the 
+    goal, using the temp, as needed.
+
+    Preconditions: 
+    start, goal, and temp are strings
+    n is an integer"""
+    #this is the base case
+    if n == 1:
+        # it's easy to move just 1 disk!
+        print("move from " + start + " to " + goal)
+        
+    else:
+    
+        # it's harder to move multiple disks. We have to first move
+        # the top n-1 disks temporarily to "temp" then move the
+        # largest disk to the target peg. Then move the remaining n-1
+        # disks back on top of thh largest @ target.
+
+        # temporarily move all but the last disk to the "other peg"
+        # how can we do this? Check the specs of THIS FUNCTION. It
+        # can do exactly this!
+        solve_hanoi(n-1, start, temp, goal)
+        
+        # move the last disk to the target peg 
+        print("move from " + start + " to " + goal)
+
+        # now move all but the last disk to the target peg
+        # again, how can we do this? we can call THIS FUNCTION
+        solve_hanoi(n-1, temp, goal, start)
+
+
+n_disks = int(input("How many disks are on your tower? "))
+print("Do this, my friend,: ")
+
+solve_hanoi(n_disks, "left", "right", "middle")
