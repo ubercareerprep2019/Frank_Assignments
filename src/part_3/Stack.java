373d1887382ede29d8449b01311268fa20990307
Omullo <foo6@cornell.edu>
2019-06-04 12:19:46 -0400

    rough implementation of stacks

---
+++ src/part_3/Stack.java
@@ -0,0 +1,49 @@
+package part_3;
+
+/**
+ * An interface which describes the methods of the stack ADT, their declarations
+ * and use The stack is a container of objects that are inserted and removed
+ * according to the Last-In-First-Out-Principle
+ */
+
+public interface Stack<E> {
+	/**
+	 * Pushes an element onto the top of the stack
+	 *
+	 * @param e, the element to be inserted
+	 */
+	void push(E e);
+
+	/**
+	 * Removes the element on top of the stack, and returns that value to the caller
+	 *
+	 * @return element removed, or null if empty
+	 */
+
+	E pop();
+
+	/**
+	 * Looks at the element at the top of the stack and returns it without
+	 * manipulating the stack
+	 *
+	 * @return top element on the stack, null if empty
+	 */
+
+	E top();
+
+	/**
+	 * checks whether the stack is empty
+	 *
+	 * @return true if stack is empty, false otherwise
+	 */
+	boolean isEmpty();
+
+	// this is just an additional helper function, not required in the assignment
+	// documentation
+	/**
+	 * returns the number of elements in the stack
+	 *
+	 * @return number of elements in the stack
+	 */
+	int size();
+}
---
+++ src/part_3/Stacks.java
@@ -0,0 +1,61 @@
+/**
+ *
+ */
+package part_3;
+
+/**
+ * @author frankomullo
+ *
+ */
+public class Stacks<E> implements Stack<E> {
+
+	/**
+	 *
+	 */
+	private E[] stack;
+	private int top = -1; // index of the top element in the stack
+
+	/** Construct a stack with a given capacity */
+	/**
+	 * This array-based implementation has a constant running time of O(1) for all
+	 * the methods implemented. space complexity is O(N)
+	 */
+
+	@Override
+	public void push(E e) throws IllegalStateException {
+		if (size() == stack.length)
+			throw new IllegalStateException("Stack is full");
+		stack[++top] = e; // increment t before storing a new item
+
+	}
+
+	@Override
+	public E pop() {
+		if (isEmpty())
+			return null;
+		E answer = stack[top];
+		stack[top] = null; // dereference to help garbage collection (learnt this in class.) this line can
+							// be ommited without changing the complexity of the program
+		top--;
+		return answer;
+	}
+
+	@Override
+	public E top() {
+		if (isEmpty()) {
+			return null;
+		}
+		return stack[top];
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return top == -1;
+	}
+
+	@Override
+	public int size() {
+		return top + 1;
+	}
+
+}
