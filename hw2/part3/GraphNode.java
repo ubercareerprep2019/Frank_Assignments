5bd4efeaf1e36dd7b8f00398085d195f6b0d62b3
Frank Otieno <foo6@cornell.edu>
2019-08-21 18:22:51 -0400

    update GraphNode

--- hw2/part3/GraphNode.java
+++ hw2/part3/GraphNode.java
@@ -7,59 +7,59 @@ import java.util.Map;
 public class GraphNode {
 	public int data;
 
-	GraphNode(int data) {
+	public GraphNode(int data) {
 		this.data = data;
 
 	}
-}
 
-class GraphWithAdjacencyList {
-	private Map<GraphNode, List<GraphNode>> adjNodes;
+	class GraphWithAdjacencyList {
+		private Map<GraphNode, List<GraphNode>> adjNodes;
 
-	void addNode(int key) {
-		adjNodes.putIfAbsent(new GraphNode(key), new ArrayList<Integer>());
-	}
+		void addNode(int key) {
+			adjNodes.putIfAbsent(new GraphNode(key), new ArrayList<Integer>());
+		}
 
-	public GraphNode getNode(int key) {
-		for (GraphNode g : adjNodes.keySet()) {
-			if (g.data == key) {
-				return g;
+		public GraphNode getNode(int key) {
+			for (GraphNode g : adjNodes.keySet()) {
+				if (g.data == key) {
+					return g;
+				}
 			}
-		}
-		return null;
-	}
-
-	void removeNode(int key) {
-		GraphNode V = new GraphNode(key);
-		adjNodes.values().stream().forEach(e -> e.remove(V)); // iterate and remove
-		adjNodes.remove(new GraphNode(key));
-	}
-
-	void addEdge(int node1, int node2) {
-		GraphNode v1 = new GraphNode(node1);
-		GraphNode v2 = new GraphNode(node2);
-		adjNodes.get(v1).add(v2);
-		adjNodes.get(v2).add(v1);
-	}
-
-	void removeEdge(int node1, int node2) {
-		GraphNode v1 = new GraphNode(node1);
-		GraphNode v2 = new GraphNode(node2);
-		List<GraphNode> ev1 = adjNodes.get(v1);
-		List<GraphNode> ev2 = adjNodes.get(v2);
-		if (ev1 != null) {
-			ev1.remove(v2);
-		}
-		if (ev2 != null) {
-			ev2.remove(v1);
+			return null;
 		}
 
+		void removeNode(int key) {
+			GraphNode V = new GraphNode(key);
+			adjNodes.values().stream().forEach(e -> e.remove(V)); // iterate and remove
+			adjNodes.remove(new GraphNode(key));
+		}
+
+		void addEdge(int node1, int node2) {
+			GraphNode v1 = new GraphNode(node1);
+			GraphNode v2 = new GraphNode(node2);
+			adjNodes.get(v1).add(v2);
+			adjNodes.get(v2).add(v1);
+		}
+
+		void removeEdge(int node1, int node2) {
+			GraphNode v1 = new GraphNode(node1);
+			GraphNode v2 = new GraphNode(node2);
+			List<GraphNode> ev1 = adjNodes.get(v1);
+			List<GraphNode> ev2 = adjNodes.get(v2);
+			if (ev1 != null) {
+				ev1.remove(v2);
+			}
+			if (ev2 != null) {
+				ev2.remove(v1);
+			}
+
+		}
+
+		List<GraphNode> getAdjNodes(int key) {
+			GraphNode n = getNode(key);
+			return adjNodes.get(n);
+
+		}
+
 	}
-
-	List<GraphNode> getAdjNodes(int key) {
-		GraphNode n = getNode(key);
-		return adjNodes.get(n);
-
-	}
-
 }
